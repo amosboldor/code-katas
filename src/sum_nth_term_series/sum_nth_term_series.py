fa12be6d3780c782e93fc7b8bc23ec086f0a9d9f
Amos Boldor <amosboldor@gmail.com>
2016-12-26 12:02:59 -0800

    testing the zero case and turned into list comprehension

--- src/sum_nth_term_series/sum_nth_term_series.py
+++ src/sum_nth_term_series/sum_nth_term_series.py
@@ -3,7 +3,8 @@
 
 def series_sum(n):
     """Return the sum of following series upto nth term(parameter)."""
-    seq = []
-    for x in range(n):
-        seq.append(1.00 / (1 + x * 3))
+    # seq = []
+    # for x in range(n):
+    #     seq.append(1.00 / (1 + x * 3))
+    seq = [1.00 / (1 + x * 3) for x in range(n)]
     return '%.2f' % sum(seq)
