525fd7493bcc3f5383d1b7e65b31fdfbb5317786
Amos Boldor <amosboldor@gmail.com>
2017-02-20 13:52:05 -0800

    add test with longer expected path

--- src/flight-paths/test_flight_paths.py
+++ src/flight-paths/test_flight_paths.py
@@ -21,3 +21,10 @@ def test_flight_path_returns_right_cities():
     """Test if flight_path returns expected cities."""
     data = flight_path('Seattle', 'New York City')
     assert 'Seattle' in data["path"] and 'New York City' in data["path"]
+
+
+def test_flight_path_returns_right_cities_seattle_dinard():
+    """Test if flight_path returns expected cities (bigger path)."""
+    data = flight_path('Seattle', 'Dinard')
+    path = ['Seattle', 'Vancouver', 'Dublin', 'East Midlands', 'Dinard']
+    assert path == data["path"]
