a34ee5f95359cec46afe68fe5c7c9562ca79d3c6
Amos Boldor <amosboldor@gmail.com>
2017-02-20 13:45:44 -0800

    add flight path function and that takes in a start city and destination city and returns path and total distance

--- src/flight-paths/flight_paths.py
+++ src/flight-paths/flight_paths.py
@@ -2,6 +2,7 @@
 
 import json
 import math
+from networkx import Graph, dijkstra_path, dijkstra_path_length
 
 
 def calculate_distance(point1, point2):
@@ -31,8 +32,35 @@ def read_format_and_return():
         airports = json.load(file)
         formated_data = {}
         for city in airports:
-            formated_data[city['city']] = {
+            formated_data[city['city'].lower()] = {
                 'lat_lon': city['lat_lon'],
                 'destination_cities': city['destination_cities']
             }
         return formated_data
+
+
+def flight_path(start_city, end_city):
+    """Generate a flight path using a graph."""
+    airports = read_format_and_return()
+    start_city = start_city.lower()
+    end_city = end_city.lower()
+    if start_city not in airports:
+        raise ValueError('{} is not a valid start city.'.format(start_city))
+    elif end_city not in airports:
+        raise ValueError('{} is not a valid destination city.'.format(end_city))
+    airports_graph = Graph()
+    for city, data in airports.items():
+        airports_graph.add_node(city)
+        for destination_city in data['destination_cities']:
+            destination_city = destination_city.lower()
+            try:
+                distance = calculate_distance(
+                    data['lat_lon'],
+                    airports[destination_city]['lat_lon']
+                )
+            except KeyError:
+                continue
+            airports_graph.add_edge(city, destination_city, weight=distance)
+    path = dijkstra_path(airports_graph, start_city, end_city)
+    distance = dijkstra_path_length(airports_graph, start_city, end_city)
+    return {"path": [city.title() for city in path], "distance": distance}
--- src/flight-paths/test_flight_paths.py
+++ src/flight-paths/test_flight_paths.py
@@ -1,16 +1,23 @@
 """Test flight_paths.py."""
 
 from flight_paths import read_format_and_return
+from flight_paths import flight_path
 
 
 def test_read_format_and_return():
     """Test read format and return."""
     data = read_format_and_return()
-    assert data['Seattle']
+    assert data['seattle']
 
 
 def test_read_format_and_return_has_correct_data():
     """Test read format and return."""
     data = read_format_and_return()
-    assert 'lat_lon' in data['Seattle']
-    assert 'destination_cities' in data['Seattle']
+    assert 'lat_lon' in data['seattle']
+    assert 'destination_cities' in data['seattle']
+
+
+def test_flight_path_returns_right_cities():
+    """Test if flight_path returns expected cities."""
+    data = flight_path('Seattle', 'New York City')
+    assert 'Seattle' in data["path"] and 'New York City' in data["path"]
